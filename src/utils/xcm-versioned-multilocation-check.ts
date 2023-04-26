c1c3af4686c8e1573beda223e3f2d3902f21bb57
Brendon Votteler <bvotteler@users.noreply.github.com>
2023-04-26 12:04:42 +0200

    feat: future proof astar xcm versioned multilocation

--- src/utils/xcm-versioned-multilocation-check.ts
+++ src/utils/xcm-versioned-multilocation-check.ts
@@ -1,5 +1,8 @@
 import { AnyApi } from "@acala-network/sdk-core";
 
+// XcmV3Junction's GeneralKey data length in bytes is 32
+export const XCM_V3_GENERAL_KEY_DATA_BYTES = 32;
+
 export const supportsV0V1Multilocation = (api: AnyApi): boolean => {
   try {
     const keys = (api.createType("XcmVersionedMultiLocation") as any)
