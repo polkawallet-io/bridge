7445037d911cf9a61de2cfb50939ac0168fe8f24
Nantian <nxiiun@gmail.com>
2024-02-28 15:41:14 +1300

    fix: fix acala wallet import (#115)

--- src/adapters/acala/acala.ts
+++ src/adapters/acala/acala.ts
@@ -1,4 +1,4 @@
-import { Wallet } from "@acala-network/sdk/wallet";
+import { Wallet } from "@acala-network/sdk/wallet/wallet";
 import { AnyApi, FixedPointNumber } from "@acala-network/sdk-core";
 import {
   catchError,
