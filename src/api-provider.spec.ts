02f10eb0369e9e85a9110b132e4472a2f751b19e
qwer951123 <qiuhao951@gmail.com>
2024-07-04 10:29:40 +0800

    feat: update release version

--- src/api-provider.spec.ts
+++ src/api-provider.spec.ts
@@ -16,7 +16,7 @@ describe("api-provider", () => {
 
     const res = await firstValueFrom(
       provider.connectFromChain(chains, {
-        karura: ["wss://karura-rpc.dwellir.com", "wss://karura.api.onfinality.io/public-ws", "wss://karura.polkawallet.io"],
+        karura: ["wss://karura-rpc.dwellir.com", "wss://karura.polkawallet.io"],
       })
     );
 
